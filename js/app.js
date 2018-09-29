d1731617edc895cd287481293554c08c7aefbb23
winstonsieck <sieck@globalcognition.org>
2018-09-29 15:48:23 -0400

    first commit

---
+++ js/app.js
@@ -0,0 +1,25 @@
+/**
+ * Main app file.  Initializes app components.
+ */
+
+
+/**
+ * The main app object.
+ *
+ */
+var vanillaPress = {};
+
+
+/**
+ * Initializes the VanillaPress app
+ * @return {void}
+ */
+vanillaPress.init = function() {
+
+  model.init();
+  router.init();
+  view.init();
+  editor.init();
+};
+
+vanillaPress.init();
